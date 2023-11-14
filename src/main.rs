8eaa32f293b0e6bba29d7954a143f5e95d732918
Amália <220939@buas.nl>
2023-11-14 16:00:33 +0100

    Raster triangle function

--- src/main.rs
+++ src/main.rs
@@ -11,6 +11,26 @@ pub struct Vertex {
     pub color: Vec3,
 }
 
+fn raster_triangle(v0: &Vertex, v1: &Vertex, v2: &Vertex, triangle_area: f32, buffer: &mut Vec<u32>, offset: Vec2) {
+    // iterating over the buffer
+    for (i, pixel) in buffer.iter_mut().enumerate()
+    {
+        let point = Vec2::new((i % WIDTH) as f32 + offset.x, (i / WIDTH) as f32 + offset.y);
+        let bary_options = barycentric_coords(point, v0.pos, v1.pos, v2.pos, triangle_area);
+        *pixel = match bary_options {
+            Some(bary) => {
+                let color = bary.x * v0.color + bary.y * v1.color + bary.z * v2.color;
+                from_u8_rgb(
+                    (color.x * 255.0) as u8,
+                    (color.y * 255.0) as u8,
+                    (color.z * 255.0) as u8,
+                )
+            }
+            _ => 0, // from_u8_rgb(255.0 as u8, 255.0 as u8, 255.0 as u8)
+        }
+    }
+}
+
 fn input_handling(window: &Window, offset: &mut Vec2) {
     let move_by: f32 = 5.0;
     if window.is_key_down(Key::A) {
@@ -55,30 +75,13 @@ fn main() {
         pos: Vec2::new(WIDTH as f32 / 3.0 * 2.0, 50.0),
         color: Vec3::new(0.0, 1.0, 1.0),
     };
+
     let triangle_area = edge_function(v0.pos, v1.pos, v2.pos);
-
     let mut offset = Vec2::new(0.0, 0.0);
-
+    
     while window.is_open() && !window.is_key_down(Key::Escape) {
         input_handling(&window, &mut offset);
-        for i in 0..(WIDTH * HEIGHT) {
-            let point = Vec2::new((i % WIDTH) as f32 + offset.x, (i / WIDTH) as f32 + offset.y);
-
-            let bary_options = barycentric_coords(point, v0.pos, v1.pos, v2.pos, triangle_area);
-            buffer[i] = match bary_options {
-                Some(bary) => {
-                    let color = bary.x * v0.color + bary.y * v1.color + bary.z * v2.color;
-                    from_u8_rgb(
-                        (color.x * 255.0) as u8,
-                        (color.y * 255.0) as u8,
-                        (color.z * 255.0) as u8,
-                    )
-                }
-                _ => 0, // from_u8_rgb(255.0 as u8, 255.0 as u8, 255.0 as u8)
-            }
-        }
-
-        // We unwrap here as we want this code to exit if it fails. Real applications may want to handle this in a different way
+        raster_triangle(&v0, &v1, &v2, triangle_area, &mut buffer, offset);
         window.update_with_buffer(&buffer, WIDTH, HEIGHT).unwrap();
     }
 }
